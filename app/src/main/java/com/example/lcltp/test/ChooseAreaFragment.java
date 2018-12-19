1277703acd8adf60e660ecb52b196696eda1c622
chenglin1.li <281913796@qq.com>
2018-12-19 16:05:27 +0800

    完成天气显示界面，手动更新天气功能及切换城市功能

--- app/src/main/java/com/example/lcltp/test/ChooseAreaFragment.java
+++ app/src/main/java/com/example/lcltp/test/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.lcltp.test;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -91,6 +92,19 @@ public class ChooseAreaFragment extends Fragment {
                 }else if(currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if(currentLevel == LEVEL_COUNTY){
+                    String weatehrId = countyList.get(position).getWeatherId();
+                    if(getActivity() instanceof MainActivity){
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatehrId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if(getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatehrId);
+                    }
                 }
             }
         });
--- app/src/main/java/com/example/lcltp/test/util/Utility.java
+++ app/src/main/java/com/example/lcltp/test/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.lcltp.test.db.City;
 import com.example.lcltp.test.db.County;
 import com.example.lcltp.test.db.Province;
+import com.example.lcltp.test.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -76,5 +78,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherReaponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
